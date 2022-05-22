6922c7fdffaac77f295285849c652e6b4d638759
DaftenP <bjeeyong99@gmail.com>
2022-05-22 18:46:24 +0900

    OpenAPI 클래스 생성 테스트코드 작성

--- app/src/main/java/com/jpl/sdp_project/functions/OpenAPI.java
+++ app/src/main/java/com/jpl/sdp_project/functions/OpenAPI.java
@@ -7,19 +7,19 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class OpenAPI {
 
-    private static String key = "7zGuKhQ2GanEnrhx7PjRWebU6x13solCNFSY9sGcISdf2IZ3VveWX71ndVka0MuHih2dMLKw1X8%2FrKX0Lo9zcw%3D%3D";
+    private static final String key = "FbibLpOcunqY9iK%2B0S4zn5XYmhAwfQ5yzZL74ykXbEq%2BQrw9w%2Fu0T0y9tu9I5cAUXWq4%2FpiRbRELSr01pbZWcA%3D%3D";
 
     public static ArrayList<ArrayList<String>> getXML(String word) {
         ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
-        String en_word = URLEncoder.encode(word);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
+        //String en_word = URLEncoder.encode(word);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
 
 
-        String queryUrl = "https://apis.data.go.kr/1471000/MdcCompDrugInfoService02/getMdcCompDrugList02?serviceKey=" + key + "&item_name=" + en_word + "&type=xml";
+        String queryUrl = "https://apis.data.go.kr/1471000/MdcCompDrugInfoService02/getMdcCompDrugList02?serviceKey=" + key + "&item_name=" + word + "&type=xml";
 
         try {
             URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
@@ -27,14 +27,14 @@ public class OpenAPI {
 
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
+            xpp.setInput(new InputStreamReader(is, StandardCharsets.UTF_8)); //inputstream 으로부터 xml 입력받기
 
             String tag;
 
             xpp.next();
             int eventType = xpp.getEventType();
 
-            ArrayList<String> tmp_arr = new ArrayList<String>();
+            ArrayList<String> tmp_arr = new ArrayList<>();
 
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 switch (eventType) {
@@ -44,8 +44,7 @@ public class OpenAPI {
                     case XmlPullParser.START_TAG:
                         tag = xpp.getName();//태그 이름 얻어오기
 
-                        if (tag.equals("item")) ;// 첫번째 검색결과
-                        else if (tag.equals("INGR_NAME")) {
+                        if (tag.equals("INGR_NAME")) {
                             xpp.next();
                             tmp_arr.add(xpp.getText());
                         } else if (tag.equals("ITEM_NAME")) {
