ba9dab22489d01fb83e7d6239a04b95583511f83
DaftenP <bjeeyong99@gmail.com>
2022-05-22 14:57:02 +0900

    Initial commit

---
+++ app/src/main/java/com/jpl/sdp_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jpl.sdp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}S
