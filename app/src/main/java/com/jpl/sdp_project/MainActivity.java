cff942e8c49e2fa0753c9afabb5540456ccee061
DaftenP <bjeeyong99@gmail.com>
2022-05-24 13:30:10 +0900

    Medicine 클래스 생성

--- app/src/main/java/com/jpl/sdp_project/MainActivity.java
+++ app/src/main/java/com/jpl/sdp_project/MainActivity.java
@@ -10,14 +10,17 @@ import com.jpl.sdp_project.functions.OpenAPI;
 public class MainActivity extends AppCompatActivity {
 
     String edit1 = "타이레놀";
-    TextView text1;
+
+    //ArrayList<Medicine> arrayList = new ArrayList<Medicine>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         text1 = (TextView) findViewById(R.id.txt1);
-         text1.setText(OpenAPI.getXML(edit1).toString());
+        //arrayList = ;
+        TextView text1 = (TextView) findViewById(R.id.txt1);
+        text1.setText(OpenAPI.getXML(edit1).get(0).ingr_name);
     }
 
 }
--- app/src/main/java/com/jpl/sdp_project/functions/OpenAPI.java
+++ app/src/main/java/com/jpl/sdp_project/functions/OpenAPI.java
@@ -7,19 +7,20 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 public class OpenAPI {
 
     private static final String key = "FbibLpOcunqY9iK%2B0S4zn5XYmhAwfQ5yzZL74ykXbEq%2BQrw9w%2Fu0T0y9tu9I5cAUXWq4%2FpiRbRELSr01pbZWcA%3D%3D";
 
-    public static ArrayList<ArrayList<String>> getXML(String word) {
-        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
-        //String en_word = URLEncoder.encode(word);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
+    public static ArrayList<Medicine> getXML(String word) {
+        ArrayList<Medicine> arr = new ArrayList<>();
+        Medicine medicine = new Medicine();
+        String en_word = URLEncoder.encode(word);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
 
 
-        String queryUrl = "https://apis.data.go.kr/1471000/MdcCompDrugInfoService02/getMdcCompDrugList02?serviceKey=" + key + "&item_name=" + word + "&type=xml";
+        String queryUrl = "https://apis.data.go.kr/1471000/MdcCompDrugInfoService02/getMdcCompDrugList02?serviceKey=" + key + "&item_name=" + en_word + "&type=xml";
 
         try {
             URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
@@ -27,14 +28,13 @@ public class OpenAPI {
 
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new InputStreamReader(is, StandardCharsets.UTF_8)); //inputstream 으로부터 xml 입력받기
+            xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
 
-            String tag;
+            String tag = "";
 
             xpp.next();
             int eventType = xpp.getEventType();
 
-            ArrayList<String> tmp_arr = new ArrayList<>();
 
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 switch (eventType) {
@@ -43,28 +43,34 @@ public class OpenAPI {
 
                     case XmlPullParser.START_TAG:
                         tag = xpp.getName();//태그 이름 얻어오기
-
-                        if (tag.equals("INGR_NAME")) {
-                            xpp.next();
-                            tmp_arr.add(xpp.getText());
-                        } else if (tag.equals("ITEM_NAME")) {
-                            xpp.next();
-                            tmp_arr.add(xpp.getText());
-                        } else if (tag.equals("ENTP_NAME")) {
-                            xpp.next();
-                            tmp_arr.add(xpp.getText());
+                        if(xpp.getName().equals("item")){
+                            medicine = new Medicine();
                         }
                         break;
 
                     case XmlPullParser.TEXT:
+                        switch (tag) {
+                            case "INGR_NAME":
+                                xpp.next();
+                                medicine.ingr_name = xpp.getText();
+                                break;
+                            case "ITEM_NAME":
+                                xpp.next();
+                                medicine.item_name = xpp.getText();
+                                break;
+                            case "ENTP_NAME":
+                                xpp.next();
+                                medicine.entp_name = xpp.getText();
+                                break;
+                        }
                         break;
 
                     case XmlPullParser.END_TAG:
                         tag = xpp.getName(); //태그 이름 얻어오기
 
                         if (tag.equals("item")) {
-                            arr.add(tmp_arr);
-                            tmp_arr.clear();
+                            arr.add(medicine);
+                            medicine.clear();
                         }
 
                         break;
