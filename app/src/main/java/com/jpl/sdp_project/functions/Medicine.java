05e6bb384493cfa546b2d49060ee29310fc47933
DaftenP <bjeeyong99@gmail.com>
2022-05-24 13:30:28 +0900

    Medicine 클래스 생성

---
+++ app/src/main/java/com/jpl/sdp_project/functions/Medicine.java
@@ -0,0 +1,14 @@
+package com.jpl.sdp_project.functions;
+
+public class Medicine {
+    public String ingr_name;
+    public String item_name;
+    public String entp_name;
+
+    public void clear(){
+        ingr_name = "";
+        item_name = "";
+        entp_name   = "";
+    }
+
+}
