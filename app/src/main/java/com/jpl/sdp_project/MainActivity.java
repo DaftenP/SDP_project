ba228af9beb6ba55b2f94061c64c715ae2844294
lm6342 <lmg6952@gmail.com>
2022-06-13 00:41:10 +0900

    레이아웃 수정 및 리사이클러뷰 테이블박스

--- app/src/main/java/com/jpl/sdp_project/MainActivity.java
+++ app/src/main/java/com/jpl/sdp_project/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                         recyclerAdapter = new RecyclerAdapter(context, data.getItems());
                         recyclerView.setAdapter(recyclerAdapter);
 
+
                     }
 
                     @Override
